4243cd724ba070f644aa024d05ef9dd59b07ca9d
vasanth <vasanthmech1222@gmail.com>
2024-09-22 12:17:01 +0530

    exploring get,post

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
-
+const port = 8848;
 const app = express();
 
-app.use("/", (req,res) => {
-    res.send("Welcome to the first server");
+app.get("/fetchUserDetails",(req,res) => {
+    res.send({name:"vasanth",age:27,city:"kanchipuram"})
+});
+app.post("/updateUserDetails",(req,res)=>{
+    res.send("User details updated successfully")
 });
 
-app.use((req,res) => {
-    res.send("successfully first node server");
-});
 
-app.listen(8080 , () =>{
-    console.log("The port is listening in 8080...");
+// app.use("/", (req,res) => {
+//     res.send("Welcome to the first server");
+// });
+
+// app.use("/result",(req,res) => {
+//     res.send("successfully created first node server");
+// });
+
+app.listen(port , () =>{
+    console.log("The port is listening in ${port}...");
 });
